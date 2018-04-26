e556aeeb277336f266a207d84944f49a18b222e0
Krzysztof Dzido <xtofek@gmail.com>
2018-04-26 21:09:26 +0200

    [THES-14] Implement Deployment Pipeline of Config (no smoke tests, no rollback)

--- backend-config-service/src/docker/run.sh
+++ backend-config-service/src/docker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "-> Waiting for any Eureka peer to start on port $EUREKASERVICE_PORT...."
-# TODO rely on EUREKASERVICE_PORT solely
-# TODO rely on EUREKASERVICE_PORT solely
-# TODO rely on EUREKASERVICE_PORT solely
+# TODO rely on EUREKASERVICE_URI solely
+# TODO rely on EUREKASERVICE_URI solely
+# TODO rely on EUREKASERVICE_URI solely
 
 while (! `nc -z eurekapeer1 $EUREKASERVICE_PORT`) && (! `nc -z eurekapeer2 $EUREKASERVICE_PORT`); do sleep 3; done
 echo "--> Eureka service has started"
--- docker-run-config-service.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew :backend-config-service:clean :backend-config-service:build :backend-config-service:buildDockerImage && \
-    docker run -ti --rm -p 8888:8888 kdzido/thesis-configservice:latest
-
-# -ti - interactive mode (ctrl-c) to stop container
-# --rm - container will be removed after stop
