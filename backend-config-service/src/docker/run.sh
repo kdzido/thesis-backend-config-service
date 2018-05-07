9742fa1c51b6fe4101e3d1a47f67dc55ccffc098
Krzysztof Dzido <xtofek@gmail.com>
2018-05-07 21:26:57 +0200

    [THES-74] Limit JVM memory consumption in docker containers (preventing OoM)

--- backend-config-service/src/docker/run.sh
+++ backend-config-service/src/docker/run.sh
@@ -9,6 +9,9 @@ while (! `nc -z eurekapeer1 $EUREKASERVICE_PORT`) && (! `nc -z eurekapeer2 $EURE
 echo "--> Eureka service has started"
 
 echo "-> Starting Cloud Config service with Eureka endpoint: $EUREKASERVICE_URI"
-java    -Djava.security.egd=file:/dev/./urandom \
+java    -XX:+PrintFlagsFinal \
+        -XX:+UnlockExperimentalVMOptions \
+        -XX:+UseCGroupMemoryLimitForHeap \
+        -Djava.security.egd=file:/dev/./urandom \
         -Deureka.client.serviceUrl.defaultZone=$EUREKASERVICE_URI \
         -jar /usr/local/config-service/@springBootJar@
